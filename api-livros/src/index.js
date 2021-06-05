88af445109bd655d149cfe5d35688a46ecb3c78d
booli34 <luhparda34@outlook.com>
2021-06-05 00:15:56 +0000

    API Rest - Aula 2

--- api-livros/src/index.js
+++ api-livros/src/index.js
@@ -1,28 +1,72 @@
 const express = require('express'); 
+const cors = require('cors');
+const { uuid } = require('uuid');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const livros = require("./livros.json");
 
-app.get('/', (request, response) => {
+app.get('/livros', (request, response) => {
     // const { autor } = request.query; 
-    return response.json( livros )
+
+    const { titulo } = request.query;
+
+    const resultado = titulo
+        ? livros.filter( livros => livros.titulo.includes(titulo) )
+        : livros
+    
+    return response.json(resultado);
+
 }); 
 
-app.post('/', (request, response) => {
+app.post('/livros', (request, response) => {
+
     const { titulo, autor, anoPublicacao } = request.body;
-    return response.json( { titulo: titulo, autor: autor, ano: anoPublicacao } )
+    const livro = { id: uuid(), titulo, descricao, autor, anoPublicacao };
+
+    livros.push(livro)
+
+    return response.json( livro )
 });
 
-app.put('/livros/:id', (request, response) => { 
-    const parametro = request.params; 
-    return response.json( parametro )
-}); 
+app.put('/livros/', (request, response) => {
+
+    const { id, titulo, descricao, autor, anoPublicacao } = request.body;
+
+    const indiceLivro = livros.findIndex(livro => livro.id === id);
+
+    if( indiceLivro < 0 ){
+        return response.status(400).json({ error: 'Livro não encontrado, tente novamente'});
+
+    }
+
+    const livro = {
+        id,
+        titulo,
+        descricao,
+        autor,
+        anoPublicacao
+    }
+
+    livros[indiceLivro] = livro;
+
+    return response.json(livro);
+});
 
 app.delete('/livros/:id', (request, response) => {
     const { id } = request.params; 
-    return response.json( id )
+
+    const indiceLivro = livros.findIndex( livro => livro.id === Number(id));
+
+    if( indiceLivro < 0 ){
+        return response.status(400).json({ error: 'Livro não encontrado, tente novamente...'});
+    }
+
+    livros.splice(indiceLivro, 1);
+
+    return response.status(204).send();
 });
 
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
